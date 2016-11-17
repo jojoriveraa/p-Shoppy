921dd976bcb919f8220b7413bf1aa1a39bad937f
Jorge A Rivera Ibarra <jojoriveraa@gmail.com>
2016-11-17 00:16:11 -0600

    Initial porducts model

--- myProject/Shoppy/products/models.py
+++ myProject/Shoppy/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_lenght=255)
+    description = models.CharField(max_lenght=255)
+    category = models.CharField(max_lenght=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
