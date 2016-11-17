f7c1dc1fa0abe5c53d3de3652b906d9a6bace528
Jorge A Rivera Ibarra <jojoriveraa@gmail.com>
2016-11-17 01:16:56 -0600

    configure products admin

--- myProject/Shoppy/products/admin.py
+++ myProject/Shoppy/products/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+#admin.site.register(Product)
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'description', 'price', )
+    list_filter = ('category', )
