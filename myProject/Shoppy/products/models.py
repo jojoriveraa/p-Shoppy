2e3b4dce018783ce7c31d5079ba76179813fecd3
Jorge A Rivera Ibarra <jojoriveraa@gmail.com>
2016-11-17 00:20:34 -0600

    Install products app and make migrations

--- myProject/Shoppy/products/models.py
+++ myProject/Shoppy/products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Products(models.Model):
-    name = models.CharField(max_lenght=255)
-    description = models.CharField(max_lenght=255)
-    category = models.CharField(max_lenght=255)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
